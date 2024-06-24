3f22ca1d9c0c99a4b8763bb91654179bc48788d0
DShaulov <shaulovl55@gmail.com>
2024-06-24 13:57:23 +0300

    Added components

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifypetapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
